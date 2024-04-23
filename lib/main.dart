f58cef7424229e63e507ee4c998ab1fc54469c33
Omar Rafael Toribio <106570431+OmerJuve2023@users.noreply.github.com>
2024-04-23 00:33:13 -0500

    Pregunta5AP.dart successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:appi_exam/preguntas/Pregunta3AP.dart';
 import 'package:appi_exam/preguntas/Pregunta4AP.dart';
+import 'package:appi_exam/preguntas/Pregunta5AP.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Pregunta4AP(), //MyApp1, MyTextField, MyApp, MyButton
+      home: Pregunta5AP(), //MyApp1, MyTextField, MyApp, MyButton
     ),
   );
 }
